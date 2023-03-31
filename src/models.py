1540c3c3e88d1f27f03ca0e98d90252c52f443e0
Materonlove <materonlove@gmail.com>
2023-03-31 19:06:05 +0000

    tarea terminada

--- src/models.py
+++ src/models.py
@@ -14,12 +14,27 @@ class People(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     gender = Column(String(10), nullable=False)
+    height = Column(String(10), nullable=False)
+    mass = Column(String(10), nullable=False)
+    hair_color = Column(String(10), nullable=False)
+    skin_color = Column(String(10), nullable=False)
+    eye_color = Column(String(10), nullable=False)
+    birth_year = Column(String(10), nullable=False)
+    
     pass 
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)
     
     pass 
 
@@ -27,6 +42,19 @@ class Vehicle(Base):
     __tablename__ = 'vehicle'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cost_in_credits = Column(String(250), nullable=False)
+    length = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    max_atmosphering_speed = Column(String(250), nullable=False)
+    hyperdrive_rating = Column(String(250), nullable=False)
+    MGLT = Column(String(250), nullable=False)
+    cargo_capacity = Column(String(250), nullable=False)
+    consumables = Column(String(250), nullable=False)
+    pilots = Column(String(250), nullable=False)
     
     pass 
 
